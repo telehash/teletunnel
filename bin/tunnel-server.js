2b5c6c3c1749efd2b5ef605b958f04dcbaf3e1ea
Ryan Bennett <nomad.ry@gmail.com>
2015-06-22 13:26:37 -0600

    actually use cli port input

--- bin/tunnel-server.js
+++ bin/tunnel-server.js
@@ -1,8 +1,10 @@
+#!/usr/bin/env node
+
 var teletunnel = require("../main.js")
 var argv = require("minimist")(process.argv.slice(2));
-
+console.log("sanity")
 if (!argv.p){
   console.log("must provide -p port, e.g. teletunnel-server -p 8000")
 } else {
-  teletunnel.Server(8000)
+  teletunnel.Server(argv.p)
 }
